bfaf0fd61e2859fdf104ed241d600646549236de
Jordan Pagkalinawan <39219944+JBoundless@users.noreply.github.com>
2021-08-01 10:53:17 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+import "./style.css";
+
+export default function App() {
+
+const [data,setData] = useState(null);
+const [print,setPrint] = useState(false);
+const [body, setBody] = useState(null);
+
+  function getData(val) {
+    console.warn(val.target.value)
+    setData(val.target.value)
+    setPrint(false)
+  }
+  return (
+    <div className="App">
+    <h1>Impulsive Writing</h1>
+    <p>Write the first thought that comes to your head. Every time you write something new, the previous text disappears.</p>
+    <input id="textbox" type="text" onChange={getData} />
+    <button onClick={()=>setPrint(true)} >Print Paragraph</button>
+    {
+       print?
+       <p>{data}</p>
+       :null
+     }
+    </div>
+  );
+}
+
+export default App;
